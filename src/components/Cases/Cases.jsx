36173906f342cfa25aa45da5610f78a2e5b47b5b
AlinaSmiyan <s0163615@edu.kubsu.ru>
2022-01-05 21:25:57 +0300

    athir 7.0

--- src/components/Cases/Cases.jsx
+++ src/components/Cases/Cases.jsx
@@ -3,7 +3,7 @@ import './Cases.module.css';
 
 const Cases = () => {
     return (
-        <p>Its new test version of cases + 6.0</p>
+        <p>Its new test version of cases + 7.0</p>
     );
 }
 
